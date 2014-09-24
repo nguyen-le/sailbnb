8e3d102095f9e481b5ec7e7ac3358e177c3f8fb2
Nguyen Le <nguyentle7@gmail.com>
2014-09-23 22:33:33 -0700

    fixing tables

--- db/migrate/20140923181627_create_users.rb
+++ db/migrate/20140923181627_create_users.rb
@@ -15,7 +15,7 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email, unique: true
+    #add_index :users, :email, unique: true
     add_index :users, :session_token
   end
 end
