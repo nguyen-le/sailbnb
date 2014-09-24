b16aacb772374d3aa7aa5c83b62aeaac48f5173d
Nguyen Le <nguyentle7@gmail.com>
2014-09-24 11:32:31 -0700

    starting boats index and json

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,6 +1,7 @@
 WaterBnb.Routers.Router = Backbone.Router.extend({
     initialize: function(options) {
        this.$rootEl = options.$rootEl;
+       this.navbar();
     },
     routes: {
         '' : 'index',
@@ -16,5 +17,9 @@ WaterBnb.Routers.Router = Backbone.Router.extend({
         if (this._currentView) this._currentView.remove();
         this._currentView = view;
         this.$rootEl.html(view.render().$el);
+    },
+
+    navbar: function() {
+        console.log('navbar loaded');
     }
 });
