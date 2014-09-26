1a20c1b00d48fd538af213dbe84c85ca7402da8a
Nguyen Le <nguyentle7@gmail.com>
2014-09-26 09:56:14 -0700

    image text showing on page, boat needs image in order to be created

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,5 @@
 class Boat < ActiveRecord::Base
-  validates :name, :location, :price, :size, :owner, presence: true
+  validates :name, :location, :price, :size, :owner, :images, presence: true
   belongs_to(
     :owner,
     primary_key: :id,
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,15 @@ ActiveRecord::Base.transaction do
   User.create( f_name: 'Guest', l_name:'Guester', email: 'guest', password: 'apples')
   User.create( f_name: 'John', l_name:'Doe', email: 'johndoe', password: 'apples')
   User.create( f_name: 'Jane', l_name:'Doe', email: 'janedoe', password: 'apples')
-  Boat.create( name: 'Popeye', location: 'SF', price: '100', size: '3', user_id: 1)
-  Boat.create( name: 'Trails', location: 'SF', price: '100', size: '3', user_id: 2)
-  Boat.create( name: 'Bikester', location: 'SF', price: '600', size: '6', user_id: 3)
-  Boat.create( name: 'Motorboat', location: 'SF', price: '1000', size: '10', user_id: 3)
+  b1 = Boat.create( name: 'Popeye', location: 'SF', price: '100', size: '3', user_id: 1)
+  b1.images.create(filepicker_url: 'https://www.filepicker.io/api/file/4qd0BKblSyet17bAINNp')
+  b2 = Boat.create( name: 'Trails', location: 'SF', price: '100', size: '3', user_id: 2)
+  #white yacht
+  b2.images.create(filepicker_url: 'https://www.filepicker.io/api/file/z6cSwAMS9KtMUwIWtkAb')
+  b3 = Boat.create( name: 'Bikester', location: 'SF', price: '600', size: '6', user_id: 3)
+  #black boat
+  b3.images.create(filepicker_url: 'https://www.filepicker.io/api/file/OjNoyT2WQXiEVB5VFe6V')
+  b4 = Boat.create( name: 'Motorboat', location: 'SF', price: '1000', size: '10', user_id: 3)
+  #white yacht
+  b4.images.create(filepicker_url: 'https://www.filepicker.io/api/file/mfmZYloxQSWFUY2aI8mI')
 end
