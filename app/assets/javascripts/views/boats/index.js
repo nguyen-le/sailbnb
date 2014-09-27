87387e28f9408e00ac64e8ed68d29e9bf067c044
Nguyen Le <nguyentle7@gmail.com>
2014-09-26 18:09:00 -0700

    maps working

--- app/assets/javascripts/views/boats/index.js
+++ app/assets/javascripts/views/boats/index.js
@@ -4,18 +4,15 @@ WaterBnb.Views.BoatsIndex = Backbone.CompositeView.extend({
         this.listenTo( this.collection, 'sync', this.render );
         this.listenTo( this.collection, 'add', this.addItem );
         this.$displayArea = $('.display-area');
-        if (this.collection) {
-            this.collection.each( function(boat) {
-               this.addItem(boat);
-            }.bind(this) );
-            this.render();
-        }
-        this.renderMap();
+        this.collection.each( function(boat) {
+           this.addItem(boat);
+        }.bind(this) );
     },
     render: function() {
         var content = this.template({ boats: this.collection });
         this.$el.html(content);
         this.attachSubviews();
+        this.renderMap();
         return this;
     },
     addItem: function(model) {
@@ -24,5 +21,11 @@ WaterBnb.Views.BoatsIndex = Backbone.CompositeView.extend({
         this.addSubview( '.display-area', view);
     },
     renderMap: function() {
+          var mapOptions = {
+            center: { lat: 37.4, lng: -122.5},
+            zoom: 9
+          };
+          var map = new google.maps.Map(this.$('#map-canvas')[0],
+              mapOptions);
     }
 });
