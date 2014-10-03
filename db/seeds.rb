c1f51530f362793f9792517f9faa5483f84ac432
Nguyen Le <nguyentle7@gmail.com>
2014-10-03 14:21:23 -0700

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,42 +30,42 @@ ActiveRecord::Base.transaction do
   b2.images.build(filepicker_url: 'https://www.filepicker.io/api/file/MybpUDYRS7aFdwKIbOCH')
   b2.save!
 
-  b3 = Boat.new( name: 'Tesla Model Y', lat: 37.8585, long: -122.5784, price: '1600', style: 'Budget', size: 8, user_id: 4, tagline: 'Clean, efficient, luxurious')
+  b3 = Boat.new( name: 'Tesla Model Y', lat: 37.4312, long: -122.0601, price: '1600', style: 'Budget', size: 8, user_id: 4, tagline: 'Clean, efficient, luxurious')
   b3.images.build(filepicker_url: 'https://www.filepicker.io/api/file/9Hb16TFGQRaWQJIAKGc8')
   b3.images.build(filepicker_url: 'https://www.filepicker.io/api/file/Njq2qmT6TZ2xxGrTuTXc')
   b3.save!
 
-  b4 = Boat.new( name: 'LuthorYacht', lat: 37.8070, long: -122.3995, price: '5000', style: 'Luxury', size: 10, user_id: 15, tagline: 'The Crusher')
+  b4 = Boat.new( name: 'LuthorYacht', lat: 37.4643, long: -122.1113, price: '5000', style: 'Luxury', size: 10, user_id: 15, tagline: 'The Crusher')
   b4.images.build(filepicker_url: 'https://www.filepicker.io/api/file/eLSTyE54R8eejfER1CYH')
   b4.images.build(filepicker_url: 'https://www.filepicker.io/api/file/Kp4ZH9gOQM6dKDB2xYmc')
   b4.save!
 
-  b5 = Boat.new( name: "Gatsby's", lat: 37.8070, long: -122.3995, price: '4000', style: 'Luxury', size: 10, user_id: 14, tagline: "Always a party at Gatsby's", featured: true)
+  b5 = Boat.new( name: "Gatsby's", lat: 37.5215, long: -122.2280, price: '4000', style: 'Luxury', size: 10, user_id: 14, tagline: "Always a party at Gatsby's", featured: true)
   b5.images.build(filepicker_url: 'https://www.filepicker.io/api/file/rfxbMyC2TbOfLzstStMh')
   b5.images.build(filepicker_url: 'https://www.filepicker.io/api/file/lNMQDdaySrii2ORsyFUT')
   b5.save!
 
-  b6 = Boat.new( name: "BatmoYacht", lat: 37.8070, long: -122.3995, price: '1000', style: 'Luxury', size: 4, user_id: 11, tagline: "I am the dark shark", featured: true)
+  b6 = Boat.new( name: "BatmoYacht", lat: 37.6059, long: -122.4999, price: '1000', style: 'Luxury', size: 4, user_id: 11, tagline: "I am the dark shark", featured: true)
   b6.images.build(filepicker_url: 'https://www.filepicker.io/api/file/aElIjSZSqGl8s79sRgJw')
   b6.images.build(filepicker_url: 'https://www.filepicker.io/api/file/K4pxUCdIRkGt3wDlmO81')
   b6.save!
 
-  b7 = Boat.new( name: "Ruby on Sails", lat: 37.8070, long: -122.3995, price: '18000', style: 'Luxury', size: 40, user_id: 9, tagline: "Learn you a ruby, for great good!", featured: true)
+  b7 = Boat.new( name: "Ruby on Sails", lat: 37.7613, long: -122.3832, price: '18000', style: 'Luxury', size: 40, user_id: 9, tagline: "Learn you a ruby, for great good!", featured: true)
   b7.images.build(filepicker_url: 'https://www.filepicker.io/api/file/r1urBxbBQ1F6nJdRmsdT')
   b7.images.build(filepicker_url: 'https://www.filepicker.io/api/file/7OHKJ13DSXyIU8SWUIGU')
   b7.save!
 
-  b8 = Boat.new( name: "Queen's Gambit", lat: 37.8070, long: -122.3995, price: '2600', style: 'Sail', size: 8, user_id: 12, tagline: "A true beauty")
+  b8 = Boat.new( name: "Queen's Gambit", lat: 38.0047, long: -122.9769, price: '2600', style: 'Sail', size: 8, user_id: 12, tagline: "A true beauty")
   b8.images.build(filepicker_url: 'https://www.filepicker.io/api/file/hPWYZ7OKQYqMMZ4PMepB')
   b8.images.build(filepicker_url: 'https://www.filepicker.io/api/file/xOrr8wNDR5Dtl2MclWLz')
   b8.save!
 
-  b9 = Boat.new( name: "Heisenberg", lat: 37.8070, long: -122.3995, price: '10000', style: 'Budget', size: 4, user_id: 5, tagline: "Lot's of blue sky", featured: true)
+  b9 = Boat.new( name: "Heisenberg", lat: 38.0204, long: -122.8656, price: '10000', style: 'Budget', size: 4, user_id: 5, tagline: "Lot's of blue sky", featured: true)
   b9.images.build(filepicker_url: 'https://www.filepicker.io/api/file/nKQJfurmRC215WrqHP0n')
   b9.images.build(filepicker_url: 'https://www.filepicker.io/api/file/6OaYwD0Tqifhda7FOR7m')
   b9.save!
 
-  b10 = Boat.new( name: "Iron Boat Mark V", lat: 37.8070, long: -122.3995, price: '2500', style: 'Budget', size: 4, user_id: 6, tagline: "Simply the best")
+  b10 = Boat.new( name: "Iron Boat Mark V", lat: 37.4945, long: -122.4683, price: '2500', style: 'Budget', size: 4, user_id: 6, tagline: "Simply the best")
   b10.images.build(filepicker_url: 'https://www.filepicker.io/api/file/zwADveISyOwfSAlY3qmb')
   b10.images.build(filepicker_url: 'https://www.filepicker.io/api/file/4gJSnTeyTLiycJvcLVDI')
   b10.save!
