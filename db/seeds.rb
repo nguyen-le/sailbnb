f24280d230c65cd39390e825cf96887c2c185581
Nguyen Le <nguyentle7@gmail.com>
2014-10-03 13:38:41 -0700

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 ActiveRecord::Base.transaction do
   User.create!( f_name: 'Guest', l_name:'Guester', email: 'guest', password: 'apples')
-  User.create!( f_name: 'John', l_name:'Doe', email: 'johndoe', password: 'apples')
+  User.create!( f_name: 'Popeye', l_name:'Sailor', email: 'johndoe', password: 'apples', image_url:'https://www.filepicker.io/api/file/bcxeYBz1QxCC6vmBbamP')
   User.create!( f_name: 'Jane', l_name:'Doe', email: 'janedoe', password: 'apples')
-  User.create!( f_name: 'Elon', l_name:'Musk', email: 'elonmusk', password: 'apples')
+  User.create!( f_name: 'Elon', l_name:'Musk', email: 'elonmusk', password: 'apples', image_url:'https://www.filepicker.io/api/file/eDD9cNdBRNiNT0abUus1')
   #5
-  User.create!( f_name: 'Walter', l_name:'White', email: 'heisenberg', password: 'apples')
-  User.create!( f_name: 'Tony', l_name:'Stark', email: 'irohman', password: 'apples')
+  User.create!( f_name: 'Walter', l_name:'White', email: 'heisenberg', password: 'apples', image_url:'https://www.filepicker.io/api/file/lb6dS6duQJSl4n2IEllU')
+  User.create!( f_name: 'Tony', l_name:'Stark', email: 'irohman', password: 'apples', image_url:'https://www.filepicker.io/api/file/YcwCNyvtS8eyy2rSA9gW')
   User.create!( f_name: 'Jesse', l_name:'Pinkman', email: 'jessepinky', password: 'apples')
   User.create!( f_name: 'David', l_name:'Hansson', email: 'dhh', password: 'apples')
-  User.create!( f_name: 'App', l_name:'Academy', email: 'giantrobot', password: 'apples')
+  User.create!( f_name: 'App', l_name:'Academy', email: 'giantrobot', password: 'apples', image_url:'https://www.filepicker.io/api/file/F6tvi2w5T3WpKSejIfjF')
   #10
   User.create!( f_name: 'Arthur', l_name:'Curry', email: 'aquaman', password: 'apples')
-  User.create!( f_name: 'Bruce', l_name:'Wayne', email: 'bwayne', password: 'apples')
-  User.create!( f_name: 'Oliver', l_name:'Queen', email: 'oqueen', password: 'apples')
+  User.create!( f_name: 'Bruce', l_name:'Wayne', email: 'bwayne', password: 'apples', image_url:'https://www.filepicker.io/api/file/EfnkTMjcQEurEMxP3JFv')
+  User.create!( f_name: 'Oliver', l_name:'Queen', email: 'oqueen', password: 'apples', image_url:'https://www.filepicker.io/api/file/xh4bzsGvQzWTvez3iSTc')
   User.create!( f_name: 'Barbara', l_name:'Gordon', email: 'bgordon', password: 'apples')
-  User.create!( f_name: 'Jay', l_name:'Gatsby', email: 'gatsby', password: 'apples')
+  User.create!( f_name: 'Jay', l_name:'Gatsby', email: 'gatsby', password: 'apples', image_url:'https://www.filepicker.io/api/file/vtLYoWuORh61NezMWm0e')
   #15
-  User.create!( f_name: 'Lex', l_name:'Luthor', email: 'iamlex', password: 'apples')
-  User.create!( f_name: 'Sue', l_name:'Richards', email: 'youcantseeme', password: 'apples')
+  User.create!( f_name: 'Lex', l_name:'Luthor', email: 'iamlex', password: 'apples', image_url:'https://www.filepicker.io/api/file/wuDaWNKuT6icPztaekrv')
+  User.create!( f_name: 'Sue', l_name:'Richards', email: 'youcantseeme', password: 'apples', image_url:'https://www.filepicker.io/api/file/Qn64M4exQqiUoj40uD1G')
 
   b1 = Boat.new( name: 'Popeye', lat: 37.8211, long: -122.3759, price: '100', style: 'Budget', size:  4 , user_id: 2, tagline: 'A slow and steady cruiser')
   b1.description = "This is a wonderfully small boat. If you like a cozy and well furnished yacht, this is the one for you!"
@@ -25,7 +25,7 @@ ActiveRecord::Base.transaction do
   b1.images.build(filepicker_url: 'https://www.filepicker.io/api/file/yv7ohkiHRmKP9zbIAePs')
   b1.save!
 
-  b2 = Boat.new( name: 'Trailblazer', lat: 37.8235, long: -122.517, price: '100', style: 'Budget', size: 4, user_id: 3, tagline: 'Fast, speedy, quick, dynamic')
+  b2 = Boat.new( name: 'Trailblazer', lat: 37.8235, long: -122.517, price: '100', style: 'Budget', size: 4, user_id: 16, tagline: 'Fast, speedy, quick, dynamic')
   b2.images.build(filepicker_url: 'https://www.filepicker.io/api/file/pZfx7PqQQPCqV0vDK175')
   b2.images.build(filepicker_url: 'https://www.filepicker.io/api/file/MybpUDYRS7aFdwKIbOCH')
   b2.save!
@@ -40,12 +40,12 @@ ActiveRecord::Base.transaction do
   b4.images.build(filepicker_url: 'https://www.filepicker.io/api/file/Kp4ZH9gOQM6dKDB2xYmc')
   b4.save!
 
-  b5 = Boat.new( name: "Gatsby's", lat: 37.8070, long: -122.3995, price: '4000', style: 'Luxury', size: 10, user_id: 14, tagline: "Always a party at Gatsby's", feature: true)
+  b5 = Boat.new( name: "Gatsby's", lat: 37.8070, long: -122.3995, price: '4000', style: 'Luxury', size: 10, user_id: 14, tagline: "Always a party at Gatsby's", featured: true)
   b5.images.build(filepicker_url: 'https://www.filepicker.io/api/file/rfxbMyC2TbOfLzstStMh')
   b5.images.build(filepicker_url: 'https://www.filepicker.io/api/file/lNMQDdaySrii2ORsyFUT')
   b5.save!
 
-  b6 = Boat.new( name: "BatmoYacht", lat: 37.8070, long: -122.3995, price: '1000', style: 'Luxury', size: 4, user_id: 11, tagline: "I am the dark shark")
+  b6 = Boat.new( name: "BatmoYacht", lat: 37.8070, long: -122.3995, price: '1000', style: 'Luxury', size: 4, user_id: 11, tagline: "I am the dark shark", featured: true)
   b6.images.build(filepicker_url: 'https://www.filepicker.io/api/file/aElIjSZSqGl8s79sRgJw')
   b6.images.build(filepicker_url: 'https://www.filepicker.io/api/file/K4pxUCdIRkGt3wDlmO81')
   b6.save!
@@ -55,7 +55,7 @@ ActiveRecord::Base.transaction do
   b7.images.build(filepicker_url: 'https://www.filepicker.io/api/file/7OHKJ13DSXyIU8SWUIGU')
   b7.save!
 
-  b8 = Boat.new( name: "Queen's Gambit", lat: 37.8070, long: -122.3995, price: '2600', style: 'Sail', size: 8, user_id: 12, tagline: "A true beauty", featured: true)
+  b8 = Boat.new( name: "Queen's Gambit", lat: 37.8070, long: -122.3995, price: '2600', style: 'Sail', size: 8, user_id: 12, tagline: "A true beauty")
   b8.images.build(filepicker_url: 'https://www.filepicker.io/api/file/hPWYZ7OKQYqMMZ4PMepB')
   b8.images.build(filepicker_url: 'https://www.filepicker.io/api/file/xOrr8wNDR5Dtl2MclWLz')
   b8.save!
